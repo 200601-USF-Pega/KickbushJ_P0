1d8104c56ed90329a840f70b353ec3b384767856
jamakick <jamakick@iu.edu>
2020-06-11 12:06:13 -0400

    adding hidden db login

--- src/main/java/com/jamakick/santasWorkshop/db/ConnectionManager.java
+++ src/main/java/com/jamakick/santasWorkshop/db/ConnectionManager.java
@@ -1,5 +1,31 @@
 package com.jamakick.santasWorkshop.db;
 
-public class ConnectionManager {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import com.jamakick.santasWorkshop.interfaces.ConnectionManagerInterface;
+
+public class ConnectionManager implements ConnectionManagerInterface {
+	
+	Connection connection;
+	String url;
+	String user;
+	String pwd;
+
+	@Override
+	public Connection getDBConnection() {
+		
+		try {
+			connection = DriverManager.getConnection(url, user, pwd);
+		}
+		
+		catch(SQLException e) {
+			System.out.println("Exception: " + e.getMessage());
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
 
 }
--- src/main/java/com/jamakick/santasWorkshop/interfaces/ConnectionManagerInterface.java
+++ src/main/java/com/jamakick/santasWorkshop/interfaces/ConnectionManagerInterface.java
@@ -1,5 +1,9 @@
 package com.jamakick.santasWorkshop.interfaces;
 
+import java.sql.Connection;
+
 public interface ConnectionManagerInterface {
+	
+	public Connection getDBConnection();
 
 }
