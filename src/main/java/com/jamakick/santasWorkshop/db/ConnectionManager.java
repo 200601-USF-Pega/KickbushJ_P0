23d27baa1420b59bc60ff3698e29cfec991044a1
jamakick <jamakick@iu.edu>
2020-06-16 22:46:00 -0400

    Updated log4j to log4j2

--- src/main/java/com/jamakick/santasWorkshop/db/ConnectionManager.java
+++ src/main/java/com/jamakick/santasWorkshop/db/ConnectionManager.java
@@ -9,16 +9,16 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 import org.apache.ibatis.jdbc.ScriptRunner;
-import org.apache.log4j.BasicConfigurator;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 
 public class ConnectionManager {
 	
-	static final Logger logger = Logger.getLogger(ConnectionManager.class);
+	private static final Logger logger = LogManager.getLogger(ConnectionManager.class);
 
 	public ConnectionManager() {
-		
-		BasicConfigurator.configure();
+
 		logger.debug("Creating Connection Manager");
 		
 		
